441e43d1f625e8591efd9ca20c1e788c6b050f54
Yash Kushwaha <yashkc2025@gmail.com>
2023-06-17 22:54:09 +0530

    Create members.js

---
+++ members.js
@@ -0,0 +1,24 @@
+export const members = [
+    {
+        "name": "Yash Kumar यश कुमार",
+        "email": "22f3000472@ds.study.iitm.ac.in",
+        "links" : {
+            "github": "https://github.com/yashkc2025",
+            "linkedin": "https://www.linkedin.com/in/yashkc2025/",
+            "instagram": "null",
+        },
+        "photo" : "https://avatars.githubusercontent.com/u/56161089?v=4",
+        "shortIntro" : "Hey I am Yash, a student at Indian Institute of Technology Madras. Currently, I am in freshman year of B.S. Data Science and Applications.",
+    },
+    {
+        "name" : "null",
+        "email" : "null",
+        "links" : {
+            "github" : "null",
+            "linkedin" : "null",
+            "instagram" : "null",
+        },
+        "photo" : "null",
+        "shortIntro" : "null",
+    },
+]
