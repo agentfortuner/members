72c16f1878e488857883acf333d5fe60ecdad893
Harshit Sharma <121124691+harshit-sharmaaa@users.noreply.github.com>
2023-06-17 23:40:26 +0530

    Update members.js

--- members.js
+++ members.js
@@ -13,6 +13,20 @@ export const members = [
         "position" : "Technical Head"
     },
     {
+        "name" : "Harshit Sharma",
+        "email" : "22f3000590@ds.study.iitm.ac.in",
+        "links" : {
+            "github" : "https://github.com/harshit-sharmaaa",
+            "linkedin" : "https://www.linkedin.com/in/harshit-sharma-a59b0a20b/",
+            "instagram" : "https://instagram.com/harxit_ig?igshid=MzNlNGNkZWQ4Mg==",
+            "personal-website" : "In Progress",
+        },
+        "photo" : "https://avatars.githubusercontent.com/u/121124691?s=400&u=3d31f6d514d7b13958876c6b09bfb3d26d671db8&v=4",
+        "shortIntro" : "I am an Engineering Student , FrontEnd Developer and Open Source Contributer SSOC' 23 ",
+        "position" : "Technical Executive"
+    },
+    
+    {
         "name" : "null",
         "email" : "null",
         "links" : {
